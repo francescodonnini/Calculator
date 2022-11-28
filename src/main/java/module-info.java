ed6dac793b01e496f0c34de33daee7e90c262ef9
francesco donnini <donnini.francesco00@gmail.com>
2022-11-28 23:39:59 +0100

    Initial commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module io.francescodonnini.calc {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+
+    opens io.francescodonnini.calc to javafx.fxml;
+    exports io.francescodonnini.calc;
+}
